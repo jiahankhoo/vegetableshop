bce1085fcdafa0e25cd68fb103efb9831510cfef
unknown <jiahankhoo@gmail.com>
2024-08-23 15:21:22 +0800

    account

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,17 +9,27 @@
             <div class="row">
                 @unless(count($products)==0)
                 @foreach ($products as $product)
-                <div class="col">
+                <div class="col-">
                     <div class="box_section">
                       <div class="image_4"><img src="images/img-4.png"></div>
                       <h2 class="dolor_text">$<span style="color: #ebc30a;">{{ $product->p_price }}</span></h2>
                       <h2 class="dolor_text">{{ $product->name }}</h2>
                       <h2 class="dolor_text_1">1 kg</h2>
                       <p class="tempor_text">{{ $product->p_desc }} </p>
+
+                      <!-- Mass input and Total Price Display -->
+                      <div>
+                          <button onclick="adjustMass(-100, {{ $product->id }})">-</button>
+                          <input type="number" id="mass_{{ $product->id }}" value="1000" readonly onchange="Calculate({{ $product->p_price }}, this.value / 1000)">
+                          <button onclick="adjustMass(100, {{ $product->id }})">+</button>
+                          <p>Total Price: $<span id="total_price_{{ $product->id }}"></span></p>
+                          <input type="hidden" id="total_price_input_{{ $product->id }}" value="{{ $product->p_price }}">
+                      </div>
+
                       <div class="buy_bt_1 active">
                         <form method="POST" action="/addcart/{{ $product->id }}">
                             @csrf
-                            <button type="submit" class="buy_bt_1"  >Add to cart</button>
+                            <button type="submit" class="buy_bt_1" id="addcart"  >Add to cart</button>
                         </form>
                       </div>
                     </div>
@@ -35,3 +45,20 @@
     </div>
 </div>
 @endsection
+<script>
+    function Calculate(price, mass, productId) {
+        let totalPrice = parseFloat(price) * parseFloat(mass);
+        document.getElementById('total_price_' + productId).innerText = totalPrice.toFixed(2);
+        document.getElementById('total_price_input_' + productId).value = totalPrice.toFixed(2);
+    }
+
+    function adjustMass(value, productId) {
+        let massInput = document.getElementById('mass_' + productId);
+        let newMass = parseInt(massInput.value) + value;
+        if (newMass < 100) { // Ensure at least 100g
+            newMass = 100;
+        }
+        massInput.value = newMass;
+        Calculate({{ $product->p_price }}, newMass / 1000, productId);
+    }
+</script>
