ca6a60d62cdd162e74b74538e7305f102289a390
unknown <jiahankhoo@gmail.com>
2025-07-25 15:54:38 +0800

    优化

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -35,7 +35,7 @@
   <div class="header_section">
     <div class="container-fluid">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+        <div class="logo"><a href="/home"><img src="images/logo.png"></a></div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -51,7 +51,7 @@
               <a class="nav-link" href="/orderlist">ORDERLIST</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="contact.html">CONTACT US</a>
+              <a class="nav-link" href="/contact">CONTACT US</a>
             </li>
 
 
